7408ff0f54a819e79f95ee7b480db89e9e8a40db
Keshav Yadav <kesavyadav992@gmail.com>
2024-09-17 18:12:41 +0530

    logo changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,7 +26,10 @@ function Navbar() {
 
   return (
     <div className={`navbar ${sticky ? 'dark-nav' : ''}`}>
-      <Link to='back-ground' smooth={true} offset={0} duration={100}><img src="../src/assets/image.png" alt="logo" className="logo" /></Link>
+      <Link to='back-ground' smooth={true} offset={0} duration={100}>
+      {/* <img src="../src/assets/image.png" alt="logo" className="logo" /> */}
+      <h2 className='logo-h2'>APEX</h2>
+      </Link>
 
       <ul className={mobileMenu ? '' : 'hide-menu'}>
         <li><Link to='about' smooth={true} offset={-80} duration={100}>Features</Link></li>
